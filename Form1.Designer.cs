226519f47a30e13814f5a4ff0b91963ec61c2d5c
YikangYuan <sallyooface@outlook.com>
2024-12-11 23:09:13 +0800

    完成读取.shp文件以及加载.sld样式；完成地图wmts切片（.\bin\Debug\WMTS_Tiles）；需要修改服务器监听

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.openShapefileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.layerManagerMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sliceButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openShapefileMenuItem,
+            this.layerManagerMenuItem});
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(53, 24);
+            this.menuStrip.Text = "文件";
+            // 
+            // openShapefileMenuItem
+            // 
+            this.openShapefileMenuItem.Name = "openShapefileMenuItem";
+            this.openShapefileMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openShapefileMenuItem.Text = "打开矢量图层";
+            // 
+            // layerManagerMenuItem
+            // 
+            this.layerManagerMenuItem.Name = "layerManagerMenuItem";
+            this.layerManagerMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.layerManagerMenuItem.Text = "图层管理";
+            // 
+            // sliceButton
+            // 
+            this.sliceButton.Location = new System.Drawing.Point(543, 4);
+            this.sliceButton.Name = "sliceButton";
+            this.sliceButton.Size = new System.Drawing.Size(129, 23);
+            this.sliceButton.TabIndex = 1;
+            this.sliceButton.Text = "切片";
+            this.sliceButton.UseVisualStyleBackColor = true;
+            this.sliceButton.Click += new System.EventHandler(this.sliceButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(181, 4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.sliceButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem openShapefileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem layerManagerMenuItem;
+        private System.Windows.Forms.Button sliceButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpMap.Layers;
+using SharpMap.Data.Providers;
+using SharpMap.CoordinateSystems;
+using System.Xml;
+using SharpMap.Forms;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using GeoAPI.CoordinateSystems;
+using NetTopologySuite;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private SharpMap.Forms.MapBox mapBox;
+
+        //服务器监听
+        private HttpListener httpListener;
+        private Thread listenerThread;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMap();
+            InitializeMenu();
+            var gss = new NtsGeometryServices();
+            var css = new SharpMap.CoordinateSystems.CoordinateSystemServices(
+                new CoordinateSystemFactory(),
+                new CoordinateTransformationFactory(),
+                SharpMap.Converters.WellKnownText.SpatialReference.GetAllReferenceSystems());
+
+            GeoAPI.GeometryServiceProvider.Instance = gss;
+            SharpMap.Session.Instance
+                .SetGeometryServices(gss)
+                .SetCoordinateSystemServices(css)
+                .SetCoordinateSystemRepository(css);
+            //LoadData();
+
+            //服务器
+            StartHttpServer();
+        }
+        private void InitializeMap()
+        {
+            mapBox = new SharpMap.Forms.MapBox
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(mapBox);
+        }
+        private void InitializeMenu()
+        {
+
+            openShapefileMenuItem.Click += OpenShapefileMenuItem_Click;
+
+            layerManagerMenuItem.Click += LayerManagerMenuItem_Click;
+
+
+        }
+        private void OpenShapefileMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Shapefiles (*.shp)|*.shp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string shapeFilePath = openFileDialog.FileName;
+                    string sldPath = GetSldFilePath(shapeFilePath);
+                    AddLayer(shapeFilePath, sldPath);
+                }
+            }
+        }
+        private string GetSldFilePath(string shapeFilePath)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "SLD files (*.sld)|*.sld";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+            }
+            return null;
+        }
+        private void AddLayer(string shapeFilePath, string sldPath)
+        {
+            VectorLayer vlay = new VectorLayer(System.IO.Path.GetFileNameWithoutExtension(shapeFilePath));
+            vlay.DataSource = new SharpMap.Data.Providers.ShapeFile(shapeFilePath, true);
+
+            if (!string.IsNullOrEmpty(sldPath))
+            {
+                ApplySldStyle(vlay, sldPath);
+            }
+
+            mapBox.Map.Layers.Add(vlay);
+            mapBox.Map.ZoomToExtents();
+            mapBox.Refresh();
+        }
+
+        private void ApplySldStyle(VectorLayer layer, string sldPath)
+        {
+            XmlDocument sldDoc = new XmlDocument();
+            sldDoc.Load(sldPath);
+
+            // 解析 Fill 和 Stroke 样式
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(sldDoc.NameTable);
+            nsmgr.AddNamespace("sld", "http://www.opengis.net/sld");
+            nsmgr.AddNamespace("se", "http://www.opengis.net/se");
+
+            XmlNode fillNode = sldDoc.SelectSingleNode("//sld:Fill/sld:CssParameter[@name='fill']", nsmgr);
+            if (fillNode != null)
+            {
+                string fillColor = fillNode.InnerText;
+                layer.Style.Fill = new SolidBrush(ColorTranslator.FromHtml(fillColor));
+            }
+
+            XmlNode strokeNode = sldDoc.SelectSingleNode("//sld:Stroke/sld:CssParameter[@name='stroke']", nsmgr);
+            if (strokeNode != null)
+            {
+                string strokeColor = strokeNode.InnerText;
+                layer.Style.Outline = new Pen(ColorTranslator.FromHtml(strokeColor));
+                layer.Style.EnableOutline = true;
+            }
+        }
+        private void LayerManagerMenuItem_Click(object sender, EventArgs e)
+        {
+            // 实现图层管理功能
+            LayerManagerForm layerManagerForm = new LayerManagerForm(mapBox.Map.Layers);
+            layerManagerForm.Show();
+        }
+        public class LayerManagerForm : Form
+        {
+            private CheckedListBox layerListBox;
+            private IList<ILayer> layers;
+            private MapBox mapBox;
+            private Button deleteLayerButton;
+
+            public LayerManagerForm(IList<ILayer> layers)
+            {
+                this.layers = layers;
+                //this.mapBox = mapBox;
+                InitializeComponents();
+            }
+
+            private void InitializeComponents()
+            {
+                layerListBox = new CheckedListBox
+                {
+                    Dock = DockStyle.Top,
+                    Height = 200
+                };
+
+                foreach (var layer in layers)
+                {
+                    layerListBox.Items.Add(layer.LayerName, layer.Enabled);
+                }
+
+                layerListBox.ItemCheck += LayerListBox_ItemCheck;
+                deleteLayerButton = new Button
+                {
+                    Text = "删除选中图层",
+                    Dock = DockStyle.Bottom
+                };
+                //deleteLayerButton.Click += DeleteLayerButton_Click;
+                this.Controls.Add(layerListBox);
+                //this.Controls.Add(deleteLayerButton);
+            }
+
+            private void LayerListBox_ItemCheck(object sender, ItemCheckEventArgs e)
+            {
+                layers[e.Index].Enabled = e.NewValue == CheckState.Checked;
+            }
+            private void DeleteLayerButton_Click(object sender, EventArgs e)
+            {
+                var selectedIndices = layerListBox.CheckedIndices;
+                foreach (int index in selectedIndices)
+                {
+                    layers.RemoveAt(index);
+                }
+
+                // 重新加载图层列表
+                layerListBox.Items.Clear();
+                foreach (var layer in layers)
+                {
+                    layerListBox.Items.Add(layer.LayerName, layer.Enabled);
+                }
+
+                if (mapBox != null)
+                {
+                    mapBox.Refresh();
+                }
+                else
+                {
+                    // 处理空对象的情况，例如记录错误或初始化 mapBox
+                }
+            }
+        }
+
+        private void sliceButton_Click(object sender, EventArgs e)
+        {
+            //string outputDirectory = "./WMTS_Tiles"; // 你可以根据需要更改输出目录
+            //GenerateWmtsTiles(mapBox.Map, outputDirectory);
+
+            string outputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WMTS_Tiles");
+            GenerateWmtsTiles(mapBox.Map, outputDirectory);
+        }
+        private void GenerateWmtsTiles(SharpMap.Map map, string outputDirectory)
+        {
+            int tileSize = 256; // 每个瓦片的大小
+            int zoomLevels = 5; // 生成的缩放级别数量
+            int totalTiles = 0;
+            int processedTiles = 0;
+
+            // 计算总瓦片数量
+            for (int zoom = 0; zoom < zoomLevels; zoom++)
+            {
+                double resolution = map.Envelope.Width / (tileSize * Math.Pow(2, zoom));
+                int tilesX = (int)Math.Ceiling(map.Envelope.Width / (tileSize * resolution));
+                int tilesY = (int)Math.Ceiling(map.Envelope.Height / (tileSize * resolution));
+                totalTiles += tilesX * tilesY;
+            }
+
+            progressBar.Maximum = totalTiles;
+
+            for (int zoom = 0; zoom < zoomLevels; zoom++)
+            {
+                double resolution = map.Envelope.Width / (tileSize * Math.Pow(2, zoom));
+                int tilesX = (int)Math.Ceiling(map.Envelope.Width / (tileSize * resolution));
+                int tilesY = (int)Math.Ceiling(map.Envelope.Height / (tileSize * resolution));
+
+
+                for (int x = 0; x < tilesX; x++)
+                {
+                    for (int y = 0; y < tilesY; y++)
+                    {
+                        var tileEnvelope = new GeoAPI.Geometries.Envelope(
+                            map.Envelope.MinX + x * tileSize * resolution,
+                            map.Envelope.MinX + (x + 1) * tileSize * resolution,
+                            map.Envelope.MinY + y * tileSize * resolution,
+                            map.Envelope.MinY + (y + 1) * tileSize * resolution);
+
+                        var tileMap = new SharpMap.Map(new Size(tileSize, tileSize))
+                        {
+                            SRID = map.SRID,
+                            //BackgroundLayer = map.BackgroundLayer,
+                            BackColor = map.BackColor
+                        };
+
+                        foreach (var layer in map.Layers)
+                        {
+                            tileMap.Layers.Add(layer);
+                        }
+
+                        tileMap.ZoomToBox(tileEnvelope);
+
+                        using (var bitmap = new Bitmap(tileSize, tileSize))
+                        {
+                            using (var graphics = Graphics.FromImage(bitmap))
+                            {
+                                tileMap.RenderMap(graphics);
+                            }
+
+                            string tilePath = Path.Combine(outputDirectory, $"{zoom}/{x}/{y}.png");
+                            Directory.CreateDirectory(Path.GetDirectoryName(tilePath));
+                            bitmap.Save(tilePath, System.Drawing.Imaging.ImageFormat.Png);
+                            //bitmap.Save("D:/大三上/网络基础与WebGIS/小组作业/4.webGIS服务器/WindowsFormsApp4/WMTS_Tiles/1.png", System.Drawing.Imaging.ImageFormat.Png);
+                        }
+
+                        processedTiles++;
+                        progressBar.Value = processedTiles;
+                    }
+                }
+            }
+            MessageBox.Show("切片完成！");
+
+
+        }
+        private void StartHttpServer()
+        {
+            httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:8080/");
+            listenerThread = new Thread(new ThreadStart(ListenForRequests));
+            listenerThread.Start();
+        }
+
+        private void ListenForRequests()
+        {
+            httpListener.Start();
+            while (true)
+            {
+                HttpListenerContext context = httpListener.GetContext();
+                ProcessRequest(context);
+            }
+        }
+
+        private void ProcessRequest(HttpListenerContext context)
+        {
+            string url = context.Request.Url.AbsolutePath;
+            string[] parts = url.Split('/');
+            if (parts.Length == 4 && parts[1] == "tiles")
+            {
+                int zoom = int.Parse(parts[2]);
+                int x = int.Parse(parts[3]);
+                int y = int.Parse(parts[4]);
+
+                string tilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WMTS_Tiles", $"{zoom}/{x}/{y}.png");
+                if (File.Exists(tilePath))
+                {
+                    context.Response.ContentType = "image/png";
+                    using (FileStream fs = File.OpenRead(tilePath))
+                    {
+                        fs.CopyTo(context.Response.OutputStream);
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            context.Response.Close();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            httpListener.Stop();
+            listenerThread.Abort();
+            base.OnFormClosing(e);
+        }
+    }
+}
